82c6c13ef3289e3d24e24ee3226e244d1b0353a9
Srivani R <srivanir41@gmail.com>
2025-12-04 23:21:05 +0530

    Add files via upload

---
+++ SquarePattern.java
@@ -0,0 +1,23 @@
+package com;
+
+ public class SquarePattern 
+ {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= n; j++) {
+
+                // Border or diagonals (X mark)
+                if (i == 1 || i == n || j == 1 || j == n || i == j || j == (n - i + 1)) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  "); // TWO SPACES to keep square shape
+                }
+            }
+
+            System.out.println();
+        }
+    }
+} 
